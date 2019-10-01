461efe4335db46a68a61ecbaef3f415370dd5d9d
Christian de Guzman <43079911+christiandguzman@users.noreply.github.com>
2019-09-30 21:37:09 -0600

    Add files via upload

---
+++ precache-manifest.8be6f81ecd2c8c627161c502949c1e5d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b71905d1f948900a5644366e70c56025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8167eca02b11e6697e73",
+    "url": "/static/css/main.473359cf.chunk.css"
+  },
+  {
+    "revision": "7958071614ba45495c4f",
+    "url": "/static/js/2.e00f133f.chunk.js"
+  },
+  {
+    "revision": "8167eca02b11e6697e73",
+    "url": "/static/js/main.03146411.chunk.js"
+  },
+  {
+    "revision": "edf77e470791c6b64f1c",
+    "url": "/static/js/runtime~main.0be9e7ee.js"
+  },
+  {
+    "revision": "f796edb92830910b1746703df15965e1",
+    "url": "/static/media/background.f796edb9.jpg"
+  },
+  {
+    "revision": "07aa5a579cb243e2b0e0166b5dc1ca8e",
+    "url": "/static/media/christian-2.07aa5a57.jpg"
+  },
+  {
+    "revision": "40cf2215973f8014678a4d86c77aa83e",
+    "url": "/static/media/christian.40cf2215.jpg"
+  },
+  {
+    "revision": "8170743c0c8b0aa60750b65a138c74a1",
+    "url": "/static/media/debadman.8170743c.PNG"
+  },
+  {
+    "revision": "a9c4d3ed8104f73ad77cfbe106841a6b",
+    "url": "/static/media/online-store.a9c4d3ed.PNG"
+  }
+]);
